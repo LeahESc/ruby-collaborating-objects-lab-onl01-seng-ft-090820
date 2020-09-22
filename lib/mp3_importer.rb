41d54fd96a9c54aeccbb17fe224807b3334328cc
Leah Schlackman <leahemily1@gmail.com>
2020-09-22 21:03:25 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,6 +8,6 @@ class MP3Importer
   def files 
   file_dir = Dir["/spec/fixtures/mp3s/*"]
   # file_dir
-  Dir.glob('path/**/*').select { |e| File.file? e }
+  Dir.glob('path/**/*').reject { |e| File.directory? e }
   end
 end 
