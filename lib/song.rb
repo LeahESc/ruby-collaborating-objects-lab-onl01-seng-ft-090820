5d1170e995a1520f5a9d83d877a8e92406e324dc
Leah Schlackman <leahemily1@gmail.com>
2020-09-22 22:20:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,8 +14,9 @@ class Song
   def self.new_by_filename(filename)
     song_array = filename.split(" - ")
     song = self.new(song_array[1])
-    artist.name = song_array[0]
-    song.artist = Artist.find_or_create_by_name(artist.name) 
+    # artist.name = song_array[0]
+    song.artist_name = song_array[0]
+    song
   end
   
   def artist_name=(name) 
