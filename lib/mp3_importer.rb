f4298b5b621b4a6c72907eb5fdb791c758fccc75
Leah Schlackman <leahemily1@gmail.com>
2020-09-22 21:08:22 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,7 +6,7 @@ class MP3Importer
   end 
   
   def files 
-  file_dir = Dir.glob(./spec/fixtures/mp3s).reject { |e| File.directory? e }
+  file_dir = Dir.glob('./spec/fixtures/mp3s').reject { |e| File.directory? e }
   file_dir
   end
 end 
