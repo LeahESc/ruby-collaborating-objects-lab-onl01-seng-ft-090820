0cf213b285487becdff14e38a553ddd062abab1f
Leah Schlackman <leahemily1@gmail.com>
2020-09-22 22:08:50 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -20,7 +20,7 @@ class MP3Importer
   end
   
   def import 
-    files.each do |file| Song.new_by_file_name(file.gsub(".mp3","").split(" - ")
+    files.each do |file| Song.new_by_file_name(file.gsub(".mp3","").split(" - "))
   end
     
 end 
